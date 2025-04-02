8f864184e0d170334d15103d9cbb8523aa626a4c
VityaVitalich <vvmoskvoretskiy@yandex.ru>
2025-04-02 16:25:04 +0000

    remove score

--- bin/score.sh
+++
@@ -1,6 +0,0 @@
-export ACCELERATE_CONFIG='configs/accelerate_config.yaml'
-export SCORE_CONFIG='configs/score.yaml'
-export WANDB_API_KEY=''
-
-
-accelerate launch --config_file $ACCELERATE_CONFIG score.py --config_path $SCORE_CONFIG
